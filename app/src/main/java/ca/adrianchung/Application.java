5b17b01a4a0b3737f5ff7ecc41358bc527dc8aab
Adrian Chung <adrian.c.chung@gmail.com>
2015-05-06 23:21:56 -0400

    Initial commit

---
+++ app/src/main/java/ca/adrianchung/Application.java
@@ -0,0 +1,7 @@
+package ca.adrianchung;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("You have run the gradle-java-bootstrap application!");
+    }
+}
