f370632cc56368b731f50dd9c5c26a25597c1460
Adrian Chung <adrian.c.chung@gmail.com>
2015-05-06 23:35:35 -0400

    Incorporate logback into the project

--- app/src/main/java/ca/adrianchung/Application.java
+++ app/src/main/java/ca/adrianchung/Application.java
@@ -1,7 +1,17 @@
 package ca.adrianchung;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public void run() {
+        logger.info("I am running!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("You have run the gradle-java-bootstrap application!");
+        Application application = new Application();
+        application.run();
     }
 }
